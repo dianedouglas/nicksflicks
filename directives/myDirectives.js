588b92e38c3fc988f04c998f8916cb21ff13261a
John <student@epicodus.com>
2015-10-07 14:03:46 -0700

    remove standard filter from css, remove 'attrs' from directive

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -1,5 +1,5 @@
 nicksFlicks.directive("changeMode", function() {
-    return function(scope, element, attrs) {
+    return function(scope, element) {
         element.bind("click", function() {
             element.parent().parent().parent().toggleClass("night");
             element.parent().parent().parent().toggleClass("day");
