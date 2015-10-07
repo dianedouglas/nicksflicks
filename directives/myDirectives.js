92825daa67df22d9f724b3a26a85dea04ae55739
Diane Douglas <diane.douglas1@gmail.com>
2015-10-07 12:23:08 -0700

    Add custom directive changeMode for night and day color schemes. Include night and day classes in styles.css

---
+++ directives/myDirectives.js
@@ -0,0 +1,8 @@
+nicksFlicks.directive("changeMode", function() {
+  return function(scope, element, attrs) {
+          element.bind("click", function() {
+              element.parent().parent().parent().toggleClass("night");
+              element.parent().parent().parent().toggleClass("day");
+          });
+        }
+});
