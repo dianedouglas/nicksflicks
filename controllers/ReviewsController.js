6e15caf7c9fb19b00a274cd57cb949f82a9f10c6
Diane Douglas <diane.douglas1@gmail.com>
2015-10-07 12:27:39 -0700

    Refactor, fix indentation, add a couple comments

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -3,6 +3,7 @@ nicksFlicks.controller('ReviewsCtrl', function ReviewsCtrl($scope, $stateParams,
     $scope.ReviewsFactory = ReviewsFactory;
     $scope.currentReview = ReviewsFactory.findById($stateParams.reviewId);
 
+    //could have gone in the factory too
     $scope.addReview = function () {
         $scope.reviews.push({ title: $scope.newReviewTitle, reviewBody: $scope.newReviewBody, id: $scope.reviews.length });
         $scope.newReviewTitle = null;
