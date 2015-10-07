c184c15bf9e7a2d6f78b433a77a862d9cf4f4b42
Diane Douglas <diane.douglas1@gmail.com>
2015-10-07 10:55:32 -0700

    Add function and form for creating a new review.

--- controllers/ReviewsController.js
+++ controllers/ReviewsController.js
@@ -2,4 +2,11 @@ nicksFlicks.controller('ReviewsCtrl', function ReviewsCtrl($scope, $stateParams,
     $scope.reviews = ReviewsFactory.reviews;
     $scope.ReviewsFactory = ReviewsFactory;
     $scope.currentReview = ReviewsFactory.findById($stateParams.reviewId);
+
+    $scope.addReview = function () {
+        $scope.reviews.push({ title: $scope.newReviewTitle, reviewBody: $scope.newReviewBody, id: $scope.reviews.length });
+        $scope.newReviewTitle = null;
+        $scope.newReviewBody = null;
+    };
+
 });
